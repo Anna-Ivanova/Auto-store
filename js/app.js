7f211196b9f2b9558f898db67b098bffbfc4410d
Anna-Ivanova <aivanova0519@gmail.com>
2023-01-04 23:07:33 +0200

    index (add script slider), in app.js del slider

--- js/app.js
+++ js/app.js
@@ -1,48 +1,4 @@
-let imgIndex = 0;
-const width = 100;
-const images = document.querySelectorAll('.img');
-const slider = document.querySelector('.slide');
 
-showSlides();
-function showSlides() {
-    imgIndex++;
-    if (imgIndex >= images.length) {
-        imgIndex = 0;
-    }
-    moveSlide();
-    timer = setTimeout(showSlideNext, 3000);
-
-}
-
-function showSlideNext() {
-    clearTimeout(timer);
-    imgIndex++;
-    if (imgIndex >= images.length) {
-        imgIndex = 0;
-    }
-    moveSlide();
-    timer = setTimeout(showSlides, 3000);
-}
-
-function showSlidePrev() {
-    clearTimeout(timer);
-    imgIndex--;
-    if (imgIndex < 0) {
-        imgIndex = images.length - 1;
-    }
-    moveSlide();
-    timer = setTimeout(showSlideNext, 3000);
-
-}
-
-const btnNext = document.querySelector('.next');
-btnNext.addEventListener('click', showSlideNext);
-const btnBack = document.querySelector('.back');
-btnBack.addEventListener('click', showSlidePrev);
-function moveSlide() {
-    slider.style.transform = `translateX(${-imgIndex * width}%)`;
-}
-//-------------end Slider------------//
 async function getCars() {
     const response = await fetch('static/cars.json');
     let data = await response.json();
