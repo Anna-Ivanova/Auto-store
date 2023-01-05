7c61d2d7fdcbb6fb266ccfae7d08b018d9e870b5
Valeria Zavadetska <Valeria.zavadetska@gmail.com>
2023-01-05 11:45:53 +0200

    create forms

--- js/app.js
+++ js/app.js
@@ -51,4 +51,49 @@ async function getCars() {
 
 }
 
+// форма і кнопка додати для людей
+const btnAddPeople = document.querySelector('.addNewPeople');
+btnAddPeople.addEventListener('click', createNewPeople);
+
+function createNewPeople(){
+    const form = document.querySelector('.editAddPeople');
+    form.style.display = 'block';
+
+    const btnClose = document.querySelector('.btnClose');
+    btnClose.addEventListener('click', closeForm);
+}
+
+// форма і кнопка додати для товарів для машини
+const btnAddAccessories = document.querySelector('.addNewAccessories');
+btnAddAccessories.addEventListener('click', createNewAccessories);
+
+function createNewAccessories(){
+    const form = document.querySelector('.editAddGoods');
+    form.style.display = 'block';
+
+    const btnClose = document.querySelector('.btnCloseGoods');
+    btnClose.addEventListener('click', closeForm);
+}
+
+// форма і кнопка додати для машини
+const btnAddCar = document.querySelector('.addCar');
+btnAddCar.addEventListener('click', createNewCar);
+
+function createNewCar(){
+    const form = document.querySelector('.editAddCar');
+    form.style.display = 'block';
+    
+    const btnClose = document.querySelector('.btnCloseCar');
+    btnClose.addEventListener('click', closeForm);
+}
+
+// кнопка закрити
+function closeForm(){
+    const formPeople = document.querySelector('.editAddPeople');
+    formPeople.style.display = 'none';
+    const formAccessories = document.querySelector('.editAddGoods');
+    formAccessories.style.display = 'none';
+    const formCar = document.querySelector('.editAddCar');
+    formCar.style.display = 'none';
+}
 
