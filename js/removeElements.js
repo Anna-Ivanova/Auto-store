ab1fe80f792e1ee17f42a616040234990347d989
Anna-Ivanova <aivanova0519@gmail.com>
2023-01-10 17:22:06 +0200

    add removeElem add hover add class in showElem

---
+++ js/removeElements.js
@@ -0,0 +1,33 @@
+function removeCars(event) {
+    const carId = event.target.parentNode.getAttribute('id');
+    if (window.confirm(`Do you really want to remove car model ${carId}?`)) {
+        const index = carInf.findIndex(item => item.id === carId);
+        carInf.splice(index, 1);
+        localStorage.setItem('cars', JSON.stringify(carInf));
+        showCars();
+    }
+}
+function removeGoods(event) {
+    const goodId = event.target.parentNode.getAttribute('id');
+
+    if (window.confirm(`Do you really want to remove position ${goodId}?`)) {
+        const index = goodsInf.findIndex(item => item.id === +goodId);
+        goodsInf.splice(index, 1);
+        localStorage.setItem('accessories', JSON.stringify(goodsInf));
+        console.log(goodsInf);
+        showGoods();
+    }
+}
+
+function removePeople(event) {
+    const peopleId = event.target.parentNode.getAttribute('id');
+
+    if (window.confirm(`Do you really want to remove customer ${peopleId}?`)) {
+        const index = peopleInf.findIndex(item => item.id === +peopleId);
+        peopleInf.splice(index, 1);
+        localStorage.setItem('people', JSON.stringify(peopleInf));
+        console.log(peopleInf);
+        showPeople();
+    }
+
+}
