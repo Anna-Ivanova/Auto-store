d421c1745c6f5d0de8c1c974e10f8ec5abdd9242
Valeria Zavadetska <Valeria.zavadetska@gmail.com>
2023-01-16 17:43:41 +0200

    fixed error

--- js/editElements.js
+++ js/editElements.js
@@ -45,6 +45,7 @@ function saveNewEditCar(event){
     const newPrice = document.querySelector('.textPriceCar').value;
     const newQuantity = document.querySelector('.textQuantityCars').value;
 
+    cars.id = `p${newModel}`;
     cars.model = newModel;
     cars.body = newBody;
     cars.color = newColor;
